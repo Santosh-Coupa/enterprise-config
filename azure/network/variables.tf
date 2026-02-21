f3fe3d36f5658f6c015961680880754540c156ab
Santosh Jadhav <santosh.jadhav@coupa.com>
2026-02-21 16:21:34 +0530

    small modifiy

--- azure/network/variables.tf
+++ azure/network/variables.tf
@@ -1,2 +1,13 @@
 variable "resource_group" { type = string }
 variable "location" { type = string }
+
+variable "vnet_name" {}
+variable "subnet_name" {}
+
+variable "address_space" {
+  type = list(string)
+}
+
+variable "subnet_prefixes" {
+  type = list(string)
+}
