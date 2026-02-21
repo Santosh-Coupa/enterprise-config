46d2487756f0cf311bcf2ebc96c0482a4d80ed7b
Santosh Jadhav <santosh.jadhav@coupa.com>
2026-02-21 16:14:51 +0530

    small modifiy

--- azure/network/main.tf
+++ azure/network/main.tf
@@ -10,5 +10,5 @@ module "network" {
   vnet_name      = var.vnet_name
   subnet_name    = var.subnet_name
   address_space  = var.address_space
-  subnet_prefix  = var.subnet_prefix
+  subnet_prefix  = var.subnet_prefixes
 }
